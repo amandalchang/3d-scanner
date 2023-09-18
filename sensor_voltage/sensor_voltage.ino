6d879f240f4072ba140c05e05928fe9a86d49dd3
Amanda Chang <amandachang@Amandas-MacBook-Air.local>
2023-09-18 08:48:43 -0400

    Outputs voltage from sensor, includes infra for servo control

--- sensor_voltage/sensor_voltage.ino
+++ sensor_voltage/sensor_voltage.ino
@@ -1,9 +1,21 @@
+// Import Libraries
+#include <Servo.h>
+
 // Pin Defs
-const int DIST_SENSE = A0
+const int PAN_SERVO = 11;
+const int TILT_SERVO = 12;
+const int DIST_SENSE = A0;
+
+int minAngle = 0; // Adjust these values so that the real-life servo actually goes to 0 degrees
+int maxAngle = 180; // and 180 degrees
+unsigned long previousSensorMillis = 0;
 long baudRate = 9600;       // NOTE1: The baudRate for sending & receiving programs must match
+
+Servo panServo; // Creating servo object
 
 void setup() {
   // put your setup code here, to run once:
+  panServo.attach(PAN_SERVO);
   pinMode(DIST_SENSE, INPUT);
 
   Serial.begin(baudRate);     // NOTE2: Set the baudRate to 115200 for faster communication
@@ -23,4 +35,12 @@ void loop() {
 
   // Optional delay to slow down the readings
   delay(1000); // 1 second delay
+
+  //   // Move the servo to 0 degrees (minimum position)
+  // panServo.write(minAngle);
+  // delay(1000); // Wait for 1 second
+
+  // // Move the servo to 180 degrees (maximum position)
+  // panServo.write(maxAngle);
+  // delay(1000); // Wait for 1 second
 }
