e5cc119835326a4dc11a609cd0c8398cad386d22
Daeyoung Kim <corydykim@gmail.com>
2023-09-23 16:22:40 -0400

    Testing for home for servo

--- scan/scan.ino
+++ scan/scan.ino
@@ -2,15 +2,15 @@
 #include <Servo.h>
 
 // Pin Defs
-const int buttonPin = 2;  // the number of the pushbutton pin
+// const int buttonPin = 2;  // the number of the pushbutton pin
 const int PAN_SERVO = 10;
 const int TILT_SERVO = 11;
 const int DIST_SENSE = A0;
 
-int minTheta = 0; // Adjust these values so that the real-life servo actually goes to 0 degrees
-int maxTheta = 180; // and 180 degrees
-int minPhi = 0;
-int maxPhi = 180;
+int minPhi = 0; // Adjust these values so that the real-life servo actually goes to 0 degrees
+int maxPhi = 180; // and 180 degrees
+int minTheta = 0;
+int maxTheta = 180;
 
 int buttonState;            // the current reading from the input pin
 int lastButtonState = LOW;  // the previous reading from the input pin
@@ -24,7 +24,7 @@ int resolution = 10; // number of degrees the sensor moves each time
 
 int baudRate = 9600;
 
-/ Creating servo object
+// Creating servo object
 Servo panServo;
 Servo tiltServo;
 
@@ -46,50 +46,53 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis(); // setting up a delay timer
+  // unsigned long currentMillis = millis(); // setting up a delay timer
   // read the state of the switch into a local variable:
-  int reading = digitalRead(buttonPin);
+  // int reading = digitalRead(buttonPin);
+  analogWrite(PAN_SERVO, 20);
   
   // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
+  // if (reading != lastButtonState) {
+  //   // reset the debouncing timer
+  //   lastDebounceTime = millis();
+  // }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
+  // if ((millis() - lastDebounceTime) > debounceDelay) {
+  //   // whatever the reading is at, it's been there for longer than the debounce
+  //   // delay, so take it as the actual current state if the button state has changed:
+  //   if (reading != buttonState) {
+  //     buttonState = reading;
 
-      if (buttonState == HIGH) {
-          for (int j = minPhi; j <= maxPhi; j += resolution){
-            for (int i = minTheta; i <= maxTheta; i += resolution){
-              if (currentMillis - prevMillis >= time_delay) {
-                prevMillis = currentMillis; // resets the count
-                panServo.write(i);
-              }
-            if (currentMillis - prevMillis >= time_delay) {
-              prevMillis = currentMillis; // resets the count
-              tiltServo.write(j);
+  //     if (buttonState == HIGH) {
+  //         for (int j = minTheta; j <= maxTheta; j += resolution){
+  //           for (int i = minPhi; i <= maxPhi; i += resolution){
+  //             if (currentMillis - prevMillis >= time_delay) {
+  //               prevMillis = currentMillis; // resets the count
+  //               panServo.write(i);
+  //             }
+  //           }
+  //           if (currentMillis - prevMillis >= time_delay) {
+  //             prevMillis = currentMillis; // resets the count
+  //             tiltServo.write(j);
       
-    }
-      }
-    }
-  }
-  lastButtonState = reading;
+  //           }
+  //         }
+  //     }
+  //   }
+  // }
+  // lastButtonState = reading;
   
   // Read the raw analog value from the distance sensor (0-1023)
-  int rawValue = analogRead(DIST_SENSE);
+  // int rawValue = analogRead(DIST_SENSE);
 
-  // Convert the raw value to voltage (0-5V)
-  float voltage = (rawValue / 1023.0) * 5.0;
+  // // Convert the raw value to voltage (0-5V)
+  // float voltage = (rawValue / 1023.0) * 5.0;
 
-  // Print the voltage value to serial
-  if ((currentMillis - prevMillis >= serial_delay) {
-    prevMillis = currentMillis
-    Serial.print("Voltage: ");
-    Serial.print(voltage, 4); // Print with 2 decimal places
-    Serial.println(" V");
+  // // Print the voltage value to serial
+  // if ((currentMillis - prevMillis) >= serial_delay) {
+  //   prevMillis = currentMillis;
+  //   Serial.print("Voltage: ");
+  //   Serial.print(voltage, 4); // Print with 2 decimal places
+  //   Serial.println(" V");
+  // }
   }
-}
